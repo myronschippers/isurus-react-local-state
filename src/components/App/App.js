02ad45e0b377a0d105cbf267ccfb31b1112e18a8
Myron <dubtwee@gmail.com>
2019-08-20 15:34:16 -0500

    react local state demonstration with input change event

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,17 +3,55 @@ import './App.css';
 import Header from '../Header/Header';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <Header />
+    state = {
+        typedValue: 'Typing a new way',
+    }
 
-          <div className="container">
-              You Typed: SOMETHING
-          </div>
-      </div>
-    );
-  }
+    // constructor(props) {
+    //     super(props);
+
+    //     this.state = {
+    //         typedValue: 'Typing Things',
+    //     };
+    // }
+
+    changeInput = (event) => {
+        // const inputValue = $(this).val();
+        console.log(event.target.value);
+        this.setState({
+            typedValue: event.target.value,
+        });
+    }
+
+    clickButton = (event) => {
+        this.setState({
+            message: this.state.message + ' Hello React',
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+
+                <div className="container">
+                    <div>
+                        <input
+                            type="text"
+                            placeholder="Type Here"
+                            onChange={this.changeInput}
+                        />
+                        <button onClick={this.clickButton}>
+                            Do Things
+                        </button>
+                    </div>
+
+                    You Typed: {this.state.typedValue}
+                    <p>{this.state.message}</p>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
